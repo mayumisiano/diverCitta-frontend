838268bca4c77087e199739e7f4195664b96a9c4
Paula Mayumi <62121416+mayumisiano@users.noreply.github.com>
2022-11-16 17:02:34 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -58,46 +58,4 @@ function App() {
 	);
 }
 
-
 export default App;
-=======
-export default App;
-=======
-import React from 'react';
-import Navbar from './components/estaticos/navbar/Navbar';
-import Footer from './components/estaticos/footer/Footer';
-import Home from './paginas/home/Home';
-import Login from './paginas/login/Login';
-import Sobre from './paginas/Sobre/Sobre'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
-import './App.css';
-
-function App() {
-  return (
-
-    <Router>
-      <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Login />} />
-
-        <Route path="/home" element={<Home />} />
-
-        <Route path="/login" element={<Login />} />
-
-        <Route path="/sobre-nos" element={<Sobre />} />
-
-        <Route path="/cadastrousuario" element={<CadastroUsuario />} />
-
-        <Route path="/login" element={<Login />} />
-
-      </Routes>
-
-      <Footer />
-    </Router>
-
-  );
-}
-export default App;
-

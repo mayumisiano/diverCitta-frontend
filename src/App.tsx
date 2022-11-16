57aeaa3a0bd33cf03d1f6f5c0bb335e917a9f29e
Juliana Nonaka Udvari <111877563+juliana-nonaka@users.noreply.github.com>
2022-11-16 16:51:09 -0300

    Criação dos componentes

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,60 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/estaticos/navbar/Navbar';
 import Footer from './components/estaticos/footer/Footer';
+import CadastroUsuario from './paginas/cadastrarUsuario/CadastrarUsuario';
+import { Grid } from '@material-ui/core';
 import Home from './paginas/home/Home';
 import Login from './paginas/login/Login';
-import Sobre from './paginas/Sobre/Sobre'
-
 import './App.css';
+import ListaTema from './components/temas/listatema/ListaTema';
+import ListaPostagem from './components/postagens/listapostagem/ListaPostagem';
+import CadastroPost from './components/postagens/cadastroPost/CadastroPost';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import { Provider } from 'react-redux';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
-  return (
+	return (
+		<Provider store={store}>
+			<ToastContainer />
+			<Router>
+				<Navbar />
+				<div style={{ minHeight: '100vh' }}>
+					<Routes>
 
-    <Router>
-      <Navbar />
-      <Routes>
+						<Route path="/" element={<Login />} />
 
-        <Route path="/" element={<Login />} />
+						<Route path="/home" element={<Home />} />
 
-        <Route path="/home" element={<Home />} />
+						<Route path="/cadastrousuario" element={<CadastroUsuario />} />
 
-        <Route path="/login" element={<Login />} />
+						<Route path="/login" element={<Login />} />
 
-        <Route path="/sobre-nos" element={<Sobre />} />
+						<Route path="/temas" element={<ListaTema />} />
 
-      </Routes>
-      <Footer />
-    </Router>    
+						<Route path="/posts" element={<ListaPostagem />} />
 
-  );
+						<Route path="/formularioPostagem" element={<CadastroPost />} />
+
+						<Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+
+						<Route path="/formularioTema" element={<CadastroTema />} />
+
+						<Route path="/formularioTema/:id" element={<CadastroTema />} />
+
+						<Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+
+						<Route path="/deletarTema/:id" element={<DeletarTema />} />
+
+					</Routes>
+				</div>
+				<Footer />
+			</Router>
+		</Provider>
+	);
 }
-          
+
 export default App;

a3ad4bf153ec14dd016552c970e6c55dc0532a31
juliana.nonaka <juliananonakaudvari@gmail.com>
2022-11-22 10:52:38 -0300

    Estilização

--- src/components/postagens/deletarPostagem/DeletarPostagem.tsx
+++ src/components/postagens/deletarPostagem/DeletarPostagem.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import React, { useEffect, useState } from 'react'
 import { Typography, Button, Card, CardActions, CardContent } from "@material-ui/core"
 import './DeletarPostagem.css';
@@ -99,4 +100,107 @@ function DeletarPostagem() {
 		</>
 	);
 }
+=======
+import React, { useEffect, useState } from 'react'
+import { Typography, Button, Card, CardActions, CardContent } from "@material-ui/core"
+import './DeletarPostagem.css';
+import { Box } from "@mui/material";
+import { buscaId, deleteId } from '../../../services/Service';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { TokenState } from '../../../store/tokens/tokensReducer';
+import { toast } from 'react-toastify';
+import Postagem from '../../../model/Postagem';
+import Container from '@mui/material/Container';
+
+function DeletarPostagem() {
+
+	let navegate = useNavigate();
+	const { id } = useParams<{ id: string }>();
+	const token = useSelector<TokenState, TokenState["tokens"]>(
+		(state) => state.tokens
+	);
+	const [post, setPosts] = useState<Postagem>()
+
+	useEffect(() => {
+		if (token == '') {
+			toast.error('Você precisa estar logado', {
+				position: 'top-right',
+				autoClose: 2000,
+				hideProgressBar: false,
+				closeOnClick: true,
+				pauseOnHover: false,
+				draggable: false,
+				theme: "light",
+				progress: undefined,
+			});
+			navegate("/login")
+		}
+	}, [token])
+
+	useEffect(() => {
+		if (id !== undefined) {
+			findById(id)
+		}
+	}, [id])
+
+	async function findById(id: string) {
+		buscaId(`/postagens/${id}`, setPosts, {
+			headers: {
+				'Authorization': token
+			}
+		})
+	}
+
+	function sim() {
+		navegate('/posts')
+		deleteId(`/postagens/${id}`, {
+			headers: {
+				'Authorization': token
+			}
+		});
+		toast.success('Postagem deletada com sucesso!', {
+			position: 'top-right',
+			autoClose: 2000,
+			hideProgressBar: false,
+			closeOnClick: true,
+			pauseOnHover: false,
+			draggable: false,
+			theme: "light",
+			progress: undefined,
+		});
+	}
+
+	function nao() {
+		navegate('/posts')
+	}
+
+	return (
+		<>
+	<Container className='back2' >			
+						<Box className= 'form1' justifyContent="center">
+							<Typography className='botoes' color="textSecondary" gutterBottom>
+								     Deseja deletar a Postagem:    
+							</Typography>
+							<Typography className='botoes' color="textSecondary" >
+								{post?.titulo}
+							</Typography>
+							<Box className='alinhar' display="flex" justifyContent="start" ml={1.0} mb={2} >
+								<Box mx={2}>
+									<Button className='botao' onClick={sim} variant="contained" size='large'>
+									Sim
+									</Button>
+							</Box>
+								<Box>
+									<Button className='botao' onClick={nao} variant="contained" size='large'>
+									Não
+									</Button>
+								</Box>
+							</Box>
+						</Box>			
+			</Container>
+		</>
+	);
+}
+>>>>>>> 3399e1199d2cb1760cf3e906a71645b98b63f4a0
 export default DeletarPostagem;
--- src/components/temas/deletarTema/DeletarTema.tsx
+++ src/components/temas/deletarTema/DeletarTema.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { Card, CardContent, Typography, CardActions, Button } from "@material-ui/core";
 import React, { ChangeEvent, useState, useEffect } from "react";
 import { Box } from "@mui/material";
@@ -108,4 +109,116 @@ function DeletarTema() {
 
 
 
+=======
+import { Card, CardContent, Typography, CardActions, Button } from "@material-ui/core";
+import React, { ChangeEvent, useState, useEffect } from "react";
+import { Box } from "@mui/material";
+import { useSelector } from "react-redux";
+import { useNavigate, useParams } from 'react-router-dom';
+import { toast } from "react-toastify";
+import { buscaId, deleteId } from "../../../services/Service";
+import "./DeletarTema.css";
+import { TokenState } from "../../../store/tokens/tokensReducer";
+import Tema from "../../../model/Tema";
+import Container from '@mui/material/Container';
+
+function DeletarTema() {
+
+    let navigate = useNavigate();
+
+    const { id } = useParams<{ id: string }>();
+
+    const [tema, setTema] = useState<Tema>();
+
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
+
+    useEffect(() => {
+        if (token == "") {
+            toast.error("Você precisa estar logade!", {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "light",
+                progress: undefined,
+            });
+            navigate("/login")
+        }
+    }, [token])
+
+    useEffect(() => {
+        if (id !== undefined) {
+            findById(id)
+        }
+    }, [id])
+
+    async function findById(id: string) {
+        buscaId(`/temas/${id}`, setTema, {
+            headers: {
+                'Authorization': token
+            }
+        })
+    }
+
+
+    function sim() {
+        navigate("/temas")
+        deleteId(`/temas/${id}`, {
+            headers: {
+                'Authorization': token
+            }
+        });
+        toast.success("Tema deletado!", {
+            position: "top-right",
+            autoClose: 2000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: false,
+            draggable: false,
+            theme: "light",
+            progress: undefined,
+        });
+    }
+
+    function nao() {
+        navigate("/temas");
+    }
+
+    return (
+        <>
+        <Container className='back2' >			
+                            <Box className= 'form1' justifyContent="center">
+                                <Typography className='botoes' color="textSecondary" gutterBottom>
+                                         Deseja excluir o Tema:    
+                                </Typography>
+                                <Typography className='botoes' color="textSecondary" >
+                                   
+                                </Typography>
+                                <Box className='alinhar' display="flex" justifyContent="start" ml={1.0} mb={2} >
+                        <Box className='alinhar' display="flex" justifyContent="start" ml={1.0} mb={2}>
+                        <Box mx={2}>
+									<Button className='botao' onClick={sim} variant="contained" size='large'>
+									Sim
+									</Button>
+							</Box>
+								<Box>
+									<Button className='botao' onClick={nao} variant="contained" size='large'>
+									Não
+									</Button>
+								</Box>
+							</Box>
+						</Box>		
+                        </Box>	
+			</Container>
+		</>
+    );
+}
+
+
+
+>>>>>>> 3399e1199d2cb1760cf3e906a71645b98b63f4a0
 export default DeletarTema;

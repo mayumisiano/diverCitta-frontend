89345b0bb1b86548ee083377114219a90e792192
Letícia Moneda <le.moneda@outlook.com>
2022-12-05 18:53:28 -0300

    feat: criando perfil de usuário

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ import ListaPostagem from './components/postagens/listaPostagem/ListaPostagem';
 import DeletarPostagem from './components/postagens/deletarPostagem/DeletarPostagem';
 import ListaTema from './components/temas/listaTema/ListaTema';
 import Sobre from './paginas/Sobre/index';
+import Perfil from './components/perfil/Perfil';
 
 
 function App() {
@@ -55,6 +56,8 @@ function App() {
 						<Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
 
 						<Route path="/deletarTema/:id" element={<DeletarTema />} />
+
+						<Route path="/perfilUsuario/" element={<Perfil/>}/>
 
 					</Routes>
 				</div>

e04b23e1e497c8aa8159783decc4cb466e6b90dc
Cristaly Medeiros <111661489+CristalyA@users.noreply.github.com>
2022-12-06 20:25:46 -0300

    Atualização carrossel

--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -7,68 +7,122 @@ import { TokenState } from "../../store/tokens/tokensReducer";
 import { toast } from "react-toastify";
 import './Home.css';
 import ModalPostagem from '../../components/postagens/modalPostagem/ModalPostagem';
+import Carousel from 'react-bootstrap/Carousel';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Home() {
 
-	let navigate = useNavigate();
-	const token = useSelector<TokenState, TokenState["tokens"]>(
-		(state) => state.tokens
-	);
+    let navigate = useNavigate();
+    const token = useSelector<TokenState, TokenState["tokens"]>(
+        (state) => state.tokens
+    );
 
-	useEffect(() => {
-		if (token === "") {
-			toast.error('Você precisa estar logado', {
-				position: "top-right",
-				autoClose: 2000,
-				hideProgressBar: false,
-				closeOnClick: true,
-				pauseOnHover: false,
-				draggable: false,
-				theme: "light",
-				progress: undefined,
-			});
-			navigate("/login")
-		}
-	}, [token])
+    useEffect(() => {
+        if (token === "") {
+            toast.error('Você precisa estar logado', {
+                position: "top-right",
+                autoClose: 2000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: false,
+                draggable: false,
+                theme: "light",
+                progress: undefined,
+            });
+            navigate("/login")
+        }
+    }, [token])
 
-	return (
-		<>
-			<Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
-				<Grid alignItems="center" item xs={6} className=".loginPage">
-					<Box paddingX={20} >
-						<Typography variant="h3"
-							gutterBottom color="textPrimary"
-							component="h3" align="center"
-							className='titulo'>
-							Sejam bem vindes à DiverCittà!</Typography>
-						<Typography variant="h5"
-							gutterBottom color="textPrimary"
-							component="h5" align="center"
-							className='titulo'>
-							Aqui falamos e apoiamos inclusão e diversidade! 
-							inclui aí: façam parte dessa mudança! </Typography>
-					</Box>
-					<Box display="flex" justifyContent="center" className='espacoBotao'>
-						<Box marginRight={1} >
-							<ModalPostagem />
-						</Box>
-						<Link to='/posts' className='text-decorator-none '>
-							<Button variant="outlined" className='botao2'>Ver Postagens</Button>
-						</Link>
-					</Box>
-				</Grid>
-				<Grid item xs={6} >
-					<img src="https://i.imgur.com/9FT6TuF.png" alt="É uma imagem de 5 crianças, uma menina negra de cabelo preto com braço esquerdo 
-					robotico de laço no cabelo amarelo camiseta branca por de baixo do vestido vestido também amarelo.Um menino branco com a 
-					perna esquerda amputada cabelo marrom claro	camiseta azul e shorts vermelho. Um menino branco de cabelo loiro de cadeira 
-					de rodas de blusa branca e calça amarela a cadeira de rodas é azul.	Uma menina branca cega de oculos escuros blusa roxa e
-					saia rosa e com uma bengala branca. E um meninobranco cabelo marrom escuro de blusa vermelha e calça marrom com a perna 
-					direita amputada  um" width="700px" height="700px" />
-				</Grid>
-				
-			</Grid>
-		</>
-	);
+    return (
+        <>
+            <Carousel className='altura'>
+
+                <Carousel.Item >
+                    <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                        <Grid alignItems="center" item xs={6} className="loginPage">
+                            <Box padding={10} >
+                                <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo'>
+                                    Sejam bem vindes à DiverCittà!</Typography>
+                                <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo'>
+                                    Aqui falamos sobre apoio, inclusão e diversidade!
+                                    Venham se incluir !
+                                    Façam parte dessa mudança! </Typography>
+                            </Box>
+                            <div className='configBotao11'>
+                                <Box display="flex" justifyContent="center" >
+                                    <Button className='botao22'> <ModalPostagem /></Button>
+                                    <Link to='/posts' className='text-decorator-none '>
+                                        <Button variant="outlined" className='botao22'>Ver Postagens</Button>
+                                    </Link>
+                                </Box>
+                            </div>
+                        </Grid>
+                        <Grid item xs={6} >
+                            <Box >
+                                <img className='imagem' src="https://i.imgur.com/2qs304K.png" alt="É uma imagem de 5 crianças, uma menina negra de cabelo preto com braço esquerdo 
+                    robotico de laço no cabelo amarelo camiseta branca por de baixo do vestido vestido também amarelo.Um menino branco com a 
+                    perna esquerda amputada cabelo marrom claro camiseta azul e shorts vermelho. Um menino branco de cabelo loiro de cadeira 
+                    de rodas de blusa branca e calça amarela a cadeira de rodas é azul. Uma menina branca cega de oculos escuros blusa roxa e
+                    saia rosa e com uma bengala branca. E um meninobranco cabelo marrom escuro de blusa vermelha e calça marrom com a perna 
+                    direita amputada  um"
+                                    width="550px" height="500px" />
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                        <Grid alignItems="center" item xs={6} className="loginPage">
+                            <Box padding={10} >
+                                <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo3'>
+                                    SOBRE A DIVERCITTÀ </Typography>
+                                <Typography variant="h5" gutterBottom color="textPrimary" component="h5" align="center" className='titulo3'>
+                                    Somos uma rede social com a missão de disponibilizar um ambiente seguro e acolhedor para interação para pessoas com Deficiência e
+                                    seus familiares. Nós do DIVERCITTÀ sabemos que a falta de inclusão tem grande impacto em nossas vidas. Assim transformando o
+                                    DIVERCITTÀ em um espaço acolhedor e seguro para se conectar. </Typography>
+                            </Box>
+                        </Grid>
+
+                        <Grid item xs={6} >
+                            <Box >
+
+                                <img className='imagem' src="https://i.imgur.com/GKvrdQJ.png" alt="" width="450px" height="500px" />
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Carousel.Item>
+
+                <Carousel.Item>
+                    <Grid container direction="row" justifyContent="center" alignItems="center" className='caixa'>
+                        <Grid alignItems="center" item xs={6} className="loginPage">
+                            <Box padding={10} >
+                                <Typography variant="h3" gutterBottom color="textPrimary" component="h3" align="center" className='titulo2'>
+                                    Contribua com nosso feed!!</Typography>
+                                <Typography variant="h5" gutterBottom color="textPrimary"component="h5" align="center"
+                                    className='titulo2'>
+                                    Esses temas já rolam aqui do Divercittà e podemos criar novos !</Typography>
+                            </Box>
+                            <Box >
+                                    <Link to='/temas' className='text-decorator-none ' >
+                                        <Button variant="outlined" className='botao20'>Ver Temas</Button>
+                                    </Link>
+                                </Box>
+                            
+
+                        </Grid>
+                        <Grid item xs={6} >
+                            <Box >
+                                <img className='imagem' src="https://i.imgur.com/pE9eVYf.png" alt="" width="450px" height="500px" />
+                            </Box>
+                        </Grid>
+                    </Grid>
+                </Carousel.Item>
+
+            </Carousel>
+
+        </>
+    );
 }
 
 export default Home;
